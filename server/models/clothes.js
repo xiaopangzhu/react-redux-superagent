0152f5741fb0dd40ba45bc48ad69d9228f3b8f95
xiaopanglzhu <1522898103@qq.com>
2016-08-22 02:17:25 +0800

    4th commit

--- server/models/clothes.js
+++ server/models/clothes.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 const url = 'mongodb://localhost:27017/myChest';
 
-exports.findAll = function(callback){
+exports.findAll = function(url,callback){
   MongoClient.connect(url, function (err, db) {
     const collection = db.collection('clothes');
     collection.find({}).toArray(function (err, docs) {
--- test/db-test.js
+++ test/db-test.js
@@ -7,8 +7,8 @@ const url = 'mongodb://localhost:27017/myChestTest';
 describe('clothes',function(){
 
   it('clothes',function(){
-    clothes.findAll(function(result){
-      expect(result).to.be.deep.equal([{"_id":0,"userName":"mahong","password":"123456","clo_list":[{"c_id":0,"season":"summer","color":"red","sort":"coat","style":"fashion","image":"0","matches":[]},{"c_id":1,"season":"summer","color":"yellow","sort":"coat","style":"fashion","image":"1","matches":[]},{"c_id":2,"season":"summer","color":"blue","sort":"pants","style":"simple","image":"2","matches":[]},{"c_id":3,"season":"summer","color":"white","sort":"pants","style":"simple","image":"3","matches":[]}]}]);
+    clothes.findAll(url,function(result){
+      expect(result).to.be.deep.equal({});
     });
   });
 });
